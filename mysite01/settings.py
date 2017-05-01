d180083338da6acb7ce50f4f18365f1c0336371a
sririshi24 <sririshi24.com>
2017-05-01 21:40:12 +0530

    host ,and so on

--- mysite01/settings.py
+++ mysite01/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e)bqd%t&63lm2uggm9%*(s7-rk^st-37%wrr2nt0p1re9r1wn3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'#for chrome book
